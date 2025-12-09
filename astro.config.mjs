4f90791327ab7b52d9365d355847aed81058c06f
Dennis Erazo <dennis.erazo96@gmail.com>
2025-12-09 12:07:49 -0600

    Cambio de adaptador por el de vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
@@ -12,7 +12,7 @@ export default defineConfig({
 
   integrations: [react()],
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
+  adapter: vercel({
+    webAnalytics: { enabled: true }
   })
 });

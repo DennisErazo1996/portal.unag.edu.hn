6a7e3d782884bf21b7964dec6662ab09a34693de
Dennis Erazo <dennis.erazo96@gmail.com>
2026-01-16 12:41:57 -0600

    Finalizacion de pagina de mision y visio e historia

---
+++ src/consts/mision-vision.ts
@@ -0,0 +1,14 @@
+const misionVision = [
+  {
+    title: "Misión",
+    description:
+      "Institución de educación superior dedicada a la gestión del conocimiento con base en criterios de calidad, pertinencia, inclusión, innovación tecnológica e internacionalización en todo su accionar universitario, con enfoque de aprender haciendo, desarrollando ciudadanos con competencias técnicas y especializadas de las ciencias agropecuarias y áreas afines; y junto a la formación en ciudadanía y convivencia social contribuye a la solución de problemas prioritarios del desarrollo sostenible nacional.",
+  },
+  {
+    title: "Visión",
+    description:
+      "Una universidad reconocida internacionalmente como una institución de educación transformadora, sustentada en una comunidad de aprendizaje diversa y comprometida con el desarrollo humano, en valores éticos y morales, demostrando calidad académica por competencias y consolidada en el uso intensivo, innovador y propositivo de la ciencia y la tecnología, teniendo como objetivo esencial la formación de liderazgos y conocimientos sobre los cuales se sustenta el desarrollo sostenible de Honduras.",
+  },
+];
+
+export default misionVision;

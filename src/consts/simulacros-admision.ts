116d42207c2beef9f621344250044d63452e9976
Dennis Erazo <dennis.erazo96@gmail.com>
2026-04-10 14:53:16 -0600

    add: simulacros de prueba en seccion de admision

---
+++ src/consts/simulacros-admision.ts
@@ -0,0 +1,45 @@
+export const simulacroMatematicas = [
+    {
+        nombre: "Matemáticas y Lógica 1",
+        url: "https://gemini.google.com/share/0278ca915e44"
+    },
+    {
+        nombre: "Matemáticas y Lógica 2",
+        url: "https://gemini.google.com/share/9413be6583be"
+    },
+    {
+        nombre: "Lógica",
+        url: "https://gemini.google.com/share/63af0693c70d"
+    },
+    {
+        nombre: "Lógica",
+        url: "https://gemini.google.com/share/35140dcb95da"
+    },
+];
+
+export const simulacroLenguaje = [
+    {
+        nombre: "Redacción Indirecta",
+        url: "https://gemini.google.com/share/7ccb5bc4e27e"
+    },
+    {
+        nombre: "Comprensión Lectora",
+        url: "https://gemini.google.com/share/606119de083f"
+    },
+    {
+        nombre: "Habilidades Lingüísticas",
+        url: "https://gemini.google.com/share/cf290eaedb97"
+    },
+];
+
+export const simulacroCiencias = [
+    {
+        nombre: "Biología",
+        url: "https://gemini.google.com/share/e8695de503c0"
+    },
+    {
+        nombre: "Química",
+        url: "https://gemini.google.com/share/7d74428c6779"
+    },
+  
+]; 

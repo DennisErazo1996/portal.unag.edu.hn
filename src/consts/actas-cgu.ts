b067d014680f2153782daa9bf2a8ac1ac8780ac5
Dennis Erazo <dennis.erazo96@gmail.com>
2026-05-08 15:49:29 -0600

    add: Pantalla de actas de reuniones e items de cgu en menu

---
+++ src/consts/actas-cgu.ts
@@ -0,0 +1,30 @@
+
+
+export const actasCgu = [
+    {
+        title: 'Acta 001-2026 | CGU',
+        date: '20 de febrero de 2026',
+        url: '/documents/cgu/acta-no-001-2026-cgu.pdf',
+    },
+    {
+        title: 'Acta 002-2026 | CGU',
+        date: '22 de febrero de 2026',
+        url: '/documents/cgu/acta-no-002-2026-cgu.pdf',
+    },
+    {
+        title: 'Acta 003-2026 | CGU',
+        date: '12 de marzo de 2026',
+        url: '/documents/cgu/acta-no-003-2026-cgu.pdf',
+    },
+    {
+        title: 'Acta 004-2026 | CGU',
+        date: '26 de marzo de 2026',
+        url: '/documents/cgu/acta-no-004-2026-cgu.pdf',
+    },
+    {
+        title: 'Acta 005-2026 | CGU',
+        date: '16 de abril de 2026',
+        url: '/documents/cgu/acta-no-005-2026-cgu.pdf',
+    },
+
+]

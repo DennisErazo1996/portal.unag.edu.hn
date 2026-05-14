38f137994400bf4e8b0c40ce80e1a61c33250fda
Dennis Erazo <dennis.erazo96@gmail.com>
2026-05-14 08:45:26 -0600

    add: cambio de adaptador en conf de astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 import autoTranslateIntegration from './src/integrations/auto-translate';
 
 const enableAutoTranslate = process.env.ENABLE_AUTO_TRANSLATE === 'true';
@@ -9,6 +10,7 @@ const enableAutoTranslate = process.env.ENABLE_AUTO_TRANSLATE === 'true';
 export default defineConfig({
   site: 'https://unag.edu.hn',
   output: 'static',
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   },

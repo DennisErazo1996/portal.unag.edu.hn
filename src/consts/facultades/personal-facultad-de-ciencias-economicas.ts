884bee80ee87eda95fc406ff973fd4c40cbab1ce
Dennis Erazo <dennis.erazo96@gmail.com>
2026-09-02 12:18:34 -0600

    chore: actualizacion de planta docente en facultad de ciencias economicas

---
+++ src/consts/facultades/personal-facultad-de-ciencias-economicas.ts
@@ -0,0 +1,27 @@
+import type { PersonalSedeRegional } from "@/types/personal-sede-regional";
+
+const personalFacultadCienciasEconomicas: PersonalSedeRegional[] = [
+  {
+    nombreCompleto: "Maykelin Fernanda Duarte",
+    nombre: "Maykelin",
+    apellido: "Duarte",
+    cargo: "Asistente de la Facultad de Ciencias Económicas y Administrativas",
+    correo: "",
+  },
+  {
+    nombreCompleto: "Iris Azucena López",
+    nombre: "Iris",
+    apellido: "López",
+    cargo: "Asistente Legal",
+    correo: "",
+  },
+  {
+    nombreCompleto: "Astrid Celeste Mendoza",
+    nombre: "Astrid",
+    apellido: "Mendoza",
+    cargo: "Asistente de Laboratorio de Alimentos",
+    correo: "",
+  },
+];
+
+export default personalFacultadCienciasEconomicas;
